be508b6690b146ecd91ef9d2bb530903e23b6b3e
Pilag6 <pilag6@gmail.com>
2023-11-07 16:35:48 +0100

    updates

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Manipulation DOM
+
+const text = document.getElementById("text")
+
+text.addEventListener("click", ()=> {
+    text.style.color = "red"
+    console.log("Now the text is red")
+})
+
