e087d07ffd703fea7c3378bfb808109359b56cf7
Pilag6 <pilag6@gmail.com>
2023-11-11 10:04:54 +0100

    Update 1111

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
 // Manipulation DOM
 
-const text = document.getElementById("text")
+const text = document.getElementById("text");
 
-text.addEventListener("click", ()=> {
-    text.style.color = "red"
-    console.log("Now the text is red")
-})
+text.addEventListener("click", () => {
+    text.style.color = "red";
+    console.log("Now the text is red");
+});
+
+
+// Intersection Observer API
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show");
+        }
+
+    });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+
+hiddenElements.forEach((element) => observer.observe(element));
 
