1dc3718ad2952cfe89e8d2828319ebb82bd723ce
Pilag6 <pilag6@gmail.com>
2023-11-11 13:10:05 +0100

    updates

--- js/main.js
+++ js/main.js
@@ -7,14 +7,6 @@ AOS.init({
     once: false,
 });
 
-// Manipulation DOM
-
-const text = document.getElementById("text");
-
-text.addEventListener("click", () => {
-    text.style.color = "red";
-    console.log("Now the text is red");
-});
 
 // Intersection Observer API
 
